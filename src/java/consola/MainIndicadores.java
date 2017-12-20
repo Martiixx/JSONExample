627b4318abe32912dd152ea7469653c61d3b7048
Martiixx <Duoc UC@B33-24>
2017-12-20 12:52:59 -0300

    se agrega lectura por teclado del indicador y se imprimen las variaciones del indicador ingresado por teclado

--- src/java/consola/MainIndicadores.java
+++ src/java/consola/MainIndicadores.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
@@ -33,5 +34,18 @@ public class MainIndicadores {
         System.out.println("Valor UF : "+ valorUF);
         System.out.println("JSON Consumido");
         
+        //Obtendremos indicadores segun entrada por teclado
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Indicador: ");
+        String indicador = teclado.next();
+        
+        System.out.println("Obteniendo variaciones de "+ indicador +"...");
+        URL url2 = new URL("http://mindicador.cl/api"+  "/"+ indicador);
+        InputStream is = url2.openStream();
+        JsonReader rdr = Json.createReader(is);
+        JsonObject obj= rdr.readObject();  
+        for(int i=0; i<obj.getJsonArray("serie").size(); i++){
+            System.out.println("valor"+(i+1)+" : "+ obj.getJsonArray("serie").getJsonObject(i).get("valor"));
+        }
     }
 }
